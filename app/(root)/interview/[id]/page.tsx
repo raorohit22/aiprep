1f849e9ad4b7939cec8e8b5d13f362e16145ce11
raorohit22 <iamrao@gmail.com>
2025-08-19 19:23:21 +0530

    issue fixed

--- app/(root)/interview/[id]/page.tsx
+++ app/(root)/interview/[id]/page.tsx
@@ -35,7 +35,7 @@ const Page = async ({ params }: RouteParams) => {
 			</div>
 
 			<Agent
-				userName={user?.name}
+				userName={user?.name || ''}
 				userId={user?.id}
 				interviewId={id}
 				type="interview"
