40a842fff2472ab520e3dbf25994fa40dd977ec8
raorohit22 <iamrao@gmail.com>
2025-08-19 21:48:50 +0530

    changes fixed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,4 @@
 import { isAuthenticated } from "@/lib/actions/auth.action";
-import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
@@ -13,13 +12,13 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
 		<div className="root-layout">
 			<nav>
 				<Link href="/" className="flex items-center gap-2">
-					<Image
+					{/* <Image
 						src="/logo.png"
 						alt="Logo"
 						width={38}
 						height={32}
 						
-					/>
+					/> */}
 					<h2 className="text-primary-100">AiPrep</h2>
 				</Link>
 			</nav>
